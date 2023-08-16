86b3bcd3d7d9a7a9979b8aa5c37afefaa293d2a4
Willy Mutua <willymathuva@gmail.com>
2023-08-16 15:50:16 +0300

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         num = ord(i)
         if num > 96 and num < 123:
             num = num - 32
-        print(chr(num), end="")
+        print("%c"% num, end="")
     print()
