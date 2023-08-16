c14489e9a629d97028aaeaf80bd4bfabb2cfb9d2
Willy Mutua <willymathuva@gmail.com>
2023-08-16 20:03:33 +0300

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    from sys import argv
+
     def infiniteadd():
-            count = len(argv)-1
-            sum = 0
-            for i in range(1, count + 1):
-                sum = sum + (int(argv[i]))
-            print("{}".format(sum))
+        count = len(argv)-1
+        sum = 0
+        for i in range(1, count + 1):
+            sum = sum + (int(argv[i]))
+        print("{}".format(sum))
 infiniteadd()
