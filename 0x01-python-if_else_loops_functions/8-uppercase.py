b94685473a3c5d08d84d3ce99c35c60326377a90
Willy Mutua <willymathuva@gmail.com>
2023-08-16 15:38:49 +0300

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        num = ord(i)
+        if num > 96 and num < 123:
+            num = num - 32
+        print(chr(num), end="")
+    print("")
