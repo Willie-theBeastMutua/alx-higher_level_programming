3383d186e019104de88d07372ad9510265afb1a0
Willy Mutua <willymathuva@gmail.com>
2023-08-28 17:19:01 +0300

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if isinstance(my_list, list):
+        leng = len(my_list)
+        copy_list = my_list
+        if idx < 0 or idx > leng:
+            return copy_list
+        else:
+            copy_list[idx] = element
+            return copy_list
