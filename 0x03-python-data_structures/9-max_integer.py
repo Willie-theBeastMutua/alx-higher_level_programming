20cf2a6473d8645a3f5309fd1a2524cdf80cbf6a
Willy Mutua <willymathuva@gmail.com>
2023-08-28 19:56:29 +0300

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    leng = len(my_list)
+    if leng < 1:
+        return None
+    j = 0
+    maxi = 0
+    while j < leng - 1:
+        if my_list[j] > maxi:
+            maxi = my_list[j]
+        j += 1
+    return maxi
