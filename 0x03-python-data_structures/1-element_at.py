d6bcb82bc02defc408a2c40742d493c1c5b7b245
Willy Mutua <willymathuva@gmail.com>
2023-08-28 16:45:44 +0300

    1. Secure access to an element in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     leng = len(my_list)
-    if idx < 0 or  idx > leng - 1:
+    if idx < 0 or idx > leng - 1:
         return None
     else:
         return my_list[idx]
