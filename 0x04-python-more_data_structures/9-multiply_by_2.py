63b8444728a3be5801c97b126746e5c402654bf8
Willy Mutua <willymathuva@gmail.com>
2023-08-29 15:24:59 +0300

    9. Multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    dic_cp = a_dictionary.copy()
+    for k, v in dic_cp.items():
+        v *= 2
+        dic_cp[k] = v
+    return dic_cp
+        
