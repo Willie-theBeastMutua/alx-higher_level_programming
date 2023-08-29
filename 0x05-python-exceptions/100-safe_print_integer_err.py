291364050e2574595a618991424fba5d38ada8e1
Willy Mutua <willymathuva@gmail.com>
2023-08-30 02:40:13 +0300

    python exception

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as v:
+        print("Exception: {}".format(v), file=sys.stderr)
+        return False
