bb1699d0896b961793cc3eea615ac2be354d9109
Willy Mutua <willymathuva@gmail.com>
2023-08-29 13:42:12 +0300

    1. Search and replace

--- 0x04-python-more_data_structures/1-main.py
+++ 0x04-python-more_data_structures/1-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 search_replace = __import__('1-search_replace').search_replace
 
-my_list = [1, 2, 3, 1, 1, 3, 6]
-new_list = search_replace(my_list, 1, 0)
+my_list = [1, 2, 3]
+new_list = search_replace(my_list, 2, 0)
 
 print(new_list)
 print(my_list)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,4 +6,4 @@ def search_replace(my_list, search, replace):
         if i == search:
             idx = new_list.index(i)
             new_list[idx] = replace
-        return new_list
+    return new_list
