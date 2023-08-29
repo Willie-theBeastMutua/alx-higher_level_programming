0ca382ad304432ae8a871a4bd11543868bd9bac7
Willy Mutua <willymathuva@gmail.com>
2023-08-29 13:38:28 +0300

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,6 +4,6 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in my_list:
         if i == search:
-            idx = my_list.index(i)
+            idx = new_list.index(i)
             new_list[idx] = replace
-    return new_list
+        return new_list
